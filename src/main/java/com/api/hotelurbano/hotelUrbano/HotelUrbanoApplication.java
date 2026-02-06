004ca22a04d40936e9cddea87d43049b6d7b6208
Nereu Vitor <nereu.vitor@aluno.ifsertao-pe.edu.br>
2026-02-06 16:56:15 -0300

    Criação do repositório do projeto

---
+++ src/main/java/com/api/hotelurbano/hotelUrbano/HotelUrbanoApplication.java
@@ -0,0 +1,13 @@
+package com.api.hotelurbano.hotelUrbano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelUrbanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelUrbanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/hotelurbano/hotelUrbano/HotelUrbanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.hotelurbano.hotelUrbano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelUrbanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
