7883084ce5f334cda4e0652e995c92dfc9e11507
Nereu Vitor <nereu.vitor@aluno.ifsertao-pe.edu.br>
2026-02-10 00:50:58 -0300

    Criação dos Repository de Quarto, Reserva e Usuario

---
+++ src/main/java/com/api/hotelurbano/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.api.hotelurbano.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.hotelurbano.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+}
