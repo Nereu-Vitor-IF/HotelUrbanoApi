f0bb753ec976808c218508543a25ee63bd020a79
Nereu Vitor <nereu.vitor@aluno.ifsertao-pe.edu.br>
2026-02-16 21:29:03 -0300

    feat: Implementa gestão de status da reserva no Service

--- src/main/java/com/api/hotelurbano/services/ReservaService.java
+++ src/main/java/com/api/hotelurbano/services/ReservaService.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.api.hotelurbano.dtos.ReservaDTO;
 import com.api.hotelurbano.models.Quarto;
 import com.api.hotelurbano.models.Reserva;
 import com.api.hotelurbano.models.Usuario;
+import com.api.hotelurbano.models.enums.StatusReserva;
 import com.api.hotelurbano.repositories.ReservaRepository;
 
 @Service
@@ -39,26 +41,29 @@ public class ReservaService {
     }
 
     @Transactional
-    public Reserva criar(Reserva obj) {
-        Usuario usuario = this.usuarioService.buscarUsuarioPorId(obj.getUsuario().getIdUsuario());
-        Quarto quarto = this.quartoService.buscarQuartoPorId(obj.getQuarto().getIdQuarto());
+    public Reserva criar(ReservaDTO dto) {
+        Usuario usuario = this.usuarioService.buscarUsuarioPorId(dto.idUsuario());
+        Quarto quarto = this.quartoService.buscarQuartoPorId(dto.idQuarto());
 
         if (!quarto.getDisponivel()) {
             throw new RuntimeException("Este quarto não está disponível para reserva!");
         }
 
         // * Enum ChonoUnit foi usado para pegar as datas no banco de dados e calcular para quantos dias a reserva foi realizada
-        long dias = ChronoUnit.DAYS.between(obj.getDataCheckin(), obj.getDataCheckout());
+        long dias = ChronoUnit.DAYS.between(dto.dataCheckIn(), dto.dataCheckOut());
 
         if (dias <= 0) {
             dias = 1;
         } 
 
-        obj.setValorTotal(dias * quarto.getPrecoDiaria());
+        Double valorTotal = dias * quarto.getPrecoDiaria();
 
-        obj.setIdReserva(null);
+        Reserva obj = new Reserva();    
         obj.setUsuario(usuario);
         obj.setQuarto(quarto);
+        obj.setDataCheckIn(dto.dataCheckIn());
+        obj.setDataCheckOut(dto.dataCheckOut());
+        obj.setValorTotal(valorTotal);
 
         obj = this.reservaRepository.save(obj);
 
@@ -69,15 +74,26 @@ public class ReservaService {
     }
 
     @Transactional
-    public void realizarCkeckout(Long id) {
-        Reserva reserva = buscarReservaPorId(id);
+    public void realizarCkeckIn(Long id) {
+        Reserva obj = this.buscarReservaPorId(id);
+        obj.setStatus(StatusReserva.CHECKIN_REALIZADO);
+        this.reservaRepository.save(obj);
+    }
+
+    @Transactional
+    public void realizarCkeckOut(Long id) {
+        Reserva obj = this.buscarReservaPorId(id);
         // * Liberação do quarto para que outro cliente possa fazer a reserva
-        this.quartoService.atualizarStatus(reserva.getQuarto().getIdQuarto(), true);
+        this.quartoService.atualizarStatus(obj.getQuarto().getIdQuarto(), true);
+
+        // * Atualiza o status da reserva
+        obj.setStatus(StatusReserva.CHECKOUT_REALIZADO);
+        this.reservaRepository.save(obj);
     }
 
     @Transactional
     public void deletar(Long id) {
-        Reserva obj = buscarReservaPorId(id);
+        Reserva obj = this.buscarReservaPorId(id);
         try {
             // * Liberação do quarto antes de apagar a reserva
             this.quartoService.atualizarStatus(obj.getQuarto().getIdQuarto(), true);
